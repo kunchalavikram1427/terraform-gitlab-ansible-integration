f5189e9abd9a55a0a78afe0e40b5fd2386f13dda
vikram <vikram@gmail.com>
2023-11-06 13:52:16 +0530

    init

--- key-pair.tf
+++ key-pair.tf
@@ -1,5 +1,5 @@
 # Register a key
 resource "aws_key_pair" "terraform-demo" {
   key_name   = "terraform-ansible-integration"
-  public_key = file("~/.ssh/id_rsa.pub") # supply a public key. ssh-keygen
+  public_key = file("./gitlab.pub") # supply a public key. ssh-keygen
 }
